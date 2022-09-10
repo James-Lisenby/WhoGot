9a70dd92f1e6c1b6212de181a94f0bc48110a5f0
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-09-10 11:52:16 -0700

    Psuedo Coded the first push of JAVA

---
+++ index.js
@@ -0,0 +1,24 @@
+// When I load the home page , I am presented with options to 'Login', go to an "About" page, or to start creating an event.
+    
+// Selecting the "About" tab will take user to an "About" page that details the group and the application.
+    // About Tab - Link to take user to "About" Page.
+        // Link will be handled with a eventlisetner.
+
+// Selecting the "Login" tab will prompt the user with the "Login Modal"
+    // "Login" button is handled with an eventlistener.
+    // User info will be authenticated and vailidated.
+        // Once info is validated, user is directed to their dashboard.
+            // Once dashboard is loaded up, all the events that the user is assosciated with will be populated onto the dashboard.
+            // 'Get' route will handle retrieving all of the assosciated info.
+
+// Entering info into the input box and selecting the "Go Plan" button will take the user to a "Sign Up" modal.
+    // User is prompted to "sign up" so that they can then continue to create their event.
+    // Once the user signs up, they are then able to finish creating the.
+        // 'Post' routes will handle creating events and posting them to the dashboard.
+
+
+
+
+// Clicking on the event "housing div" will bring up the "user event modal" containing the information for that specific event.
+
+// Clicking the "+" on the "New event" div will bring up a "New event Modal" for the user to create an event.
