c98827d5d27e54b4f00957daa1a65d08e422d9c3
Te <smoochbanner@icloud.com>
2022-09-17 10:10:20 -0700

    js update

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,33 @@
 const loginFormHandler = async (event) => {
-  event.preventDefault();
+  //login from event handler with the event passed thru
+  event.preventDefault(); // preventing default behavior of the form
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim(); //gettin the value of the email input area
+  const password = document.querySelector('#password-login').value.trim(); //getting the value of the password input area
 
   if (email && password) {
+    // if email and password are true
     const response = await fetch('/api/users/login', {
+      // fetch response data from the user login post route
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ email, password }), //stringify (format ) the password and return it in a readable format
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      // if api response status is ok , is true
+      document.location.replace('/'); // move user to the main/ profile page
     } else {
-      alert('Failed to log in.');
+      // otherwise
+      alert('Failed to log in.'); //alert client side of a failed attempt
     }
   }
 };
 
-const signupFormHandler = async (event) => {
+const signupFormHandler = async (event) => {  
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim(); 
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
